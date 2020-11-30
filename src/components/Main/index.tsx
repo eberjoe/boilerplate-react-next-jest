c80b38a973b1610ea0265c40050c3d1d0b29c2e8
Eber <eberjoe@gmail.com>
2020-11-30 01:45:15 -0300

    Add testing of styles

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo com a escrita React Avançado ao lado"
+    />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS e Styled Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Imagem de um desenvolvedor de frente para monitores com código"
+    />
+  </S.Wrapper>
 )
 
 export default Main
